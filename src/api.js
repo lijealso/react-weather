b4a0830c1b8408eb89ebf7415495fe1341b1395d
lijealso <lijealso@gmail.com>
2022-08-29 02:36:14 +0100

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '81d4170499msh66b457889e4007ep1e510ejsne5e52c3f377d', // enter your rapid api key here
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1ede2b69920a7a649655315c12b2ee9c"; // enter your key from openweather API
